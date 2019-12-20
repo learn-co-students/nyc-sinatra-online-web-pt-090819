9b8209f697b793cb2111a586feb48750d4d59eb0
Chaya Deaver <chayadeaver@gmail.com>
2019-12-20 15:59:55 -0500

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -7,12 +7,55 @@ class FiguresController < ApplicationController
   end
 
   get '/figures/new' do
-    @figures = Figure.all
+    @titles = Title.all
+    @landmarks = Landmark.all
     erb :'figures/new'
   end
 
   post '/figures' do
-    binding.pry
-    @figure = Figure.create(params)
+
+    @figure = Figure.create(params[:figure])
+    # binding.pry
+    unless params[:title][:name].empty?
+      # binding.pry
+      @figure.titles << Title.create(params[:title])
+    end
+    
+    unless params[:landmark][:name].empty?
+      
+      @figure.landmarks << Landmark.create(params[:landmark])
+    end
+    @figure.save
+    redirect to "/figures/#{@figure.id}"
+  end
+
+  get '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    erb :'/figures/show'
+  end
+
+  get '/figures/:id/edit' do
+    @figure = Figure.find(params[:id])
+    @landmarks = Landmark.all
+    @titles = Title.all
+    # binding.pry
+    erb :'/figures/edit'
+  end
+
+  patch '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    @figure.update(params[:figure])
+    # binding.pry
+    unless params[:title][:name].empty?
+      # binding.pry
+      @figure.titles << Title.create(params[:title])
+    end
+    
+    unless params[:landmark][:name].empty?
+      
+      @figure.landmarks << Landmark.create(params[:landmark])
+    end
+    @figure.save
+    redirect to "/figures/#{@figure.id}"
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191220014832) do
+ActiveRecord::Schema.define(version: 20191220144955) do
+
+  create_table "figure_titles", force: :cascade do |t|
+    t.integer "title_id"
+    t.integer "figure_id"
+  end
 
   create_table "figures", force: :cascade do |t|
     t.string "name"
