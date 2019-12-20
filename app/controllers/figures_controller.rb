1a144ff6328b09cb23eb67c50003e055f52d2259
Chaya Deaver <chayadeaver@gmail.com>
2019-12-19 21:58:54 -0500

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,18 @@
 class FiguresController < ApplicationController
   # add controller methods
+
+  get '/figures' do
+    @figures = Figure.all
+    erb :'figures/index'
+  end
+
+  get '/figures/new' do
+    @figures = Figure.all
+    erb :'figures/new'
+  end
+
+  post '/figures' do
+    binding.pry
+    @figure = Figure.create(params)
+  end
 end
